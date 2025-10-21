2d0239214deb42675ebafb8c01517e9c8a35bdc6
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-10-22 01:14:13 +0200

    lagt in funktion för problemen

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
- <div id="app">
-<Header></Header>
-
- </div>
+  <div id="app">
+    <Header></Header>
+    <Problem
+      title="Two Sum"
+      description="Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target."
+    ></Problem>
+  </div>
 </template>
 <script setup>
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
+import Problem from "./components/Problem.vue";
 </script>
 <style>
-body{
+body {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
---
+++ src/components/Problem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="problem-card">
+
+        <h2>{{ title }}</h2>
+        <p> {{ description }}</p>
+    </div>
+
+</template>
+
+<script setup>
+defineProps({
+    title:String,
+    description:String,
+
+})
+
+</script>
+<style scoped>
+.problem-card {
+  background-color: #f0f0f0;
+  padding: 15px;
+  border-radius: 8px;
+}
+</style>
