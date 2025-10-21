c6bf1fa16b98475afe66f99520b45a2bee3d0987
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-10-22 00:26:38 +0200

    börjat på header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ <div id="app">
+<Header></Header>
+
+ </div>
 </template>
+<script setup>
+import Header from './components/Header.vue';
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<header class="Header">
+    <h1>Theos leetcode lösningar</h1>
+
+</header>
+
+</template>
+<script setup>
+
+</script>
+<style scoped>
+.Header{
+    background-color: blueviolet;
+
+}
+
+
+</style>
