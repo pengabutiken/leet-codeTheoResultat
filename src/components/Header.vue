f3efb8453489409c55986783afb06f9f3188b22d
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-10-31 00:43:16 +0100

    la till en cool effekt i headern...

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,8 +21,26 @@
    font-family: "Press Start 2P", system-ui;
 
 }
-h1:hover{
-    color: black;
+h1{
+  font-size: 3em;
+  background: linear-gradient(90deg, red, orange, yellow, green, blue, indigo, violet);
+  background-size: 400%;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  transition: background-position 1s;
 }
+h1:hover {
+  animation: rainbow 2s linear infinite;
+}
+@keyframes rainbow {
+  0% {
+    background-position: 0%;
+  }
+  100% {
+    background-position: 100%;
+  }
+}
+
+
 
 </style>
